33383ed315bfece3f3306cbecfc9b17e236a9cfa
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2023-07-01 10:02:35 +0100

    feat: initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  $schema: "http://json.schemastore.org/prettierrc",
+  arrowParens: "always",
+  printWidth: 120,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+};
